18d57d4073e2966e9084d46ba3f958dc49e7eac9
RITURAJ-ARENA <majrituraj@gmail.com>
2026-05-10 06:18:55 +0530

    Characteristics of OBJECT

---
+++ JS/this.js
@@ -0,0 +1,81 @@
+console.log(this);
+
+function ranveerOnTheGlobalstage(){
+    return typeof this;
+}
+
+console.log(ranveerOnTheGlobalstage());
+
+// function ranveerWithNoScript(){
+//     "use strict"
+//     return this; // undefined
+// }
+// console.log(ranveerWithNoScript())
+
+
+// function ranveerWithNoScript(){
+ 
+//     return this;
+    // Output in browser: Window
+    // Output in Node: Global
+// }
+// console.log(ranveerWithNoScript())
+
+const bollyWoodFilm = {
+    name : "Bajirao Mastani",
+    lead : "Ranveer",
+
+    introduce(){
+        return `${this.lead} performs in ${this.name}`
+    }
+}
+console.log(bollyWoodFilm.introduce())
+//-------------------------------------------------
+//*** V V IMP
+const filmDirector= {
+    name : "Sanjay Leela Bhansali",
+    cast : ["Raveer","Deepika","Priyanka"],
+
+    announceCast(){
+        this.cast.forEach((actor) => {
+            console.log(`${this.name} introduces ${actor}`)
+        })
+    }
+}
+
+filmDirector.announceCast()
+
+
+const filmSet = {
+    crew : "Spot Boys",
+    prepareProps(){
+        console.log(`Outer this.crew : ${this.crew}`)
+        
+        function arrangeChairs(){
+            console.log(`Inner this.crew : ${this.crew}`);
+        }//Irregular nested Function does not inherit "this".
+
+        arrangeChairs();
+
+        const arrangeLights = () => {
+            console.log(`Arrow this.crew : ${this.crew}`);
+        }
+        arrangeLights();
+
+    },
+} 
+
+filmSet.prepareProps();
+
+
+// Detached Methods
+
+const actor = {
+    name : "Ranveer",
+    bow(){
+        return `${this.name} takes a bow`
+    }
+}
+console.log(actor.bow())
+const detachedBow = actor.bow
+console.log(detachedBow())
